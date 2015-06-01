8e706ffa10145969a5ff91b186e3c47fd4ff6822
Scott Brau <scottbrau30@gmail.com>
2015-05-31 21:03:39 -0700

    Setup the main-content section

--- Template/main.js
+++ Template/main.js
@@ -1,9 +1,3 @@
-$(function () {
-	$('.carousel').slick ({
-		dots: true,
-	    infinite: false,
-	    speed: 300,
-	    slidesToShow: 1,
-	    slidesToScroll: 1,
-	});
-});
+// $(function () {
+// 	$('.carousel').slick();
+// });
