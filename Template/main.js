42692639c0a9fae7537512ab3b8b1c59071acaa1
Scott Brau <scottbrau30@gmail.com>
2015-06-01 16:53:06 -0700

    changes to main.js for carousel animation

--- Template/main.js
+++ Template/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   $('.carousel').slick({
+  	arrows: false,
     dots: true,
-    adaptiveHeight: true
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000
   });
 });
 
