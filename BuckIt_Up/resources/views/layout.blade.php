0cd2908aa3d980b6e0babcde462ad8b5c77094d3
ldcudden <ldcudden@sbcglobal.net>
2015-06-01 18:17:26 -0700

    new files added

---
+++ BuckIt_Up/resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="{{ asset('/css/layout2.css') }}" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
