28b821f54c77599dd3db0690d3f1ffb2017ff56c
ldcudden <ldcudden@sbcglobal.net>
2015-06-01 21:21:08 -0700

    made style changes to login page and reskinned registration page

--- BuckIt_Up/resources/views/auth/register.blade.php
+++ BuckIt_Up/resources/views/auth/register.blade.php
@@ -1,79 +1,88 @@
-@extends('app')
+@extends('layout')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+	<div class="header">	
+		<header>
+			<nav>
+				<a class="logo" href="#">BuckIt Up</a>
+				<a class="main" href="#">Make List</a>
+				<a class="main" href="#">Find Ideas</a>
+				<a class="main" href="#">About</a>	
+				<a class="settings" href="#">Account</a>				
+				<a class="logout" href="#">Logout</a>				
+			</nav>
+		</header>		
+	</div>
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Username</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="username" value="{{ old('username') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">First Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="first_name" value="{{ old('first_name') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Last Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
-							</div>
-						</div>
-					</form>
+	<div class="reg-login">		
+		<div class="panel-body panel-default">
+			<h1>Register</h1>
+			@if (count($errors) > 0)
+				<div class="alert alert-danger">
+					<strong>Whoops!</strong> There were some problems with your input.<br><br>
+					<ul>
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
 				</div>
-			</div>
+			@endif
+
+			<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				<div class="left-form">	
+					<div class="form-group">
+						<label class="col-md-4 control-label">Username</label>
+						<div class="col-md-6">
+							<input type="text" class="textbox" name="username" value="{{ old('username') }}">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label class="col-md-4 control-label">First Name</label>
+						<div class="col-md-6">
+							<input type="text" class="textbox" name="first_name" value="{{ old('first_name') }}">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label class="col-md-4 control-label">Last Name</label>
+						<div class="col-md-6">
+							<input type="text" class="textbox" name="last_name" value="{{ old('last_name') }}">
+						</div>
+					</div>
+				</div>	
+				<div class="right-form">
+					<div class="form-group">
+						<label class="col-md-4 control-label">E-Mail Address</label>
+						<div class="col-md-6">
+							<input type="email" class="textbox" name="email" value="{{ old('email') }}">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label class="col-md-4 control-label">Password</label>
+						<div class="col-md-6">
+							<input type="password" class="textbox" name="password">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label class="col-md-4 control-label">Confirm Password</label>
+						<div class="col-md-6">
+							<input type="password" class="textbox" name="password_confirmation">
+						</div>
+					</div>
+				</div>
+				<div class="register">
+					<div class="col-md-6 col-md-offset-4">
+						<button type="submit" class="btn btn-primary">
+							Register
+						</button>
+					</div>
+				</div>	
+			</form>
 		</div>
 	</div>
-</div>
 @endsection
