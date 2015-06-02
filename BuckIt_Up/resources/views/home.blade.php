a85305369fe5b36a07ef9ee6db83d74fc4ebced7
Scott Brau <scottbrau30@gmail.com>
2015-06-02 11:16:31 -0700

    migrated home page to laravel, there are a few minor css issues to be fixed

--- BuckIt_Up/resources/views/home.blade.php
+++ BuckIt_Up/resources/views/home.blade.php
@@ -1,17 +1,62 @@
-@extends('layout')
-
-@section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					Welcome fine people, to BuckIt-Up.com and such.
-				</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BuckIt Up</title>
+	<link rel="stylesheet" href="normalize.css">
+	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Poiret+One' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+  	<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+	<link rel="stylesheet" href="/css/styles.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="slick/slick.min.js"></script>
+	<script type="text/javascript" src="/javascript/main.js"></script>
+</head>
+<body>
+	<div class="header">	
+		<header>
+			<nav>
+				<a class="logo" href="#">BuckIt Up</a>
+				<a class="main" href="#">Make List</a>
+				<a class="main" href="#">Find Ideas</a>
+				<a class="main" href="#">About</a>					
+				<button class="nav-login" type="submit">Login</button>		
+				<button class="nav-signup" type="submit">Sign Up</button>		
+			</nav>
+		</header>		
+	</div>
+	<div class="hero">
+		<div class="carousel">
+			<div><img src="images/auroraborealis.jpg" alt=""></div>
+			<div><img src="images/rio.jpg" alt=""></div>
+			<div><img src="images/greatwall.jpg" alt=""></div>
+			<div><img src="images/oregon.jpg" alt=""></div>
+			<div><img src="images/russia.jpg" alt=""></div>
+			<div><img src="images/africa.jpg" alt=""></div>
+		</div>
+		<div>
+			<h1>Your Dreams Await ...Turn Them Into Reality.</h1>
+			<div>
+				<button class="signup" type="submit">Sign Up</button>
+				<button class="login" type="submit">Login</button>		
+			</div>
+			<div class="inspire">
+				<span>Insert Inspiring Quote Here</span>
 			</div>
 		</div>
 	</div>
-</div>
-@endsection
+	<div class="main-footer">
+		<footer>
+			<div>
+				<a href="#"><i class="fa fa-twitter-square fa-3x"></i></a>
+				<a href="#"><i class="fa fa-facebook-official fa-3x"></i></a>
+				<a href="#"><i class="fa fa-instagram fa-3x"></i></a>
+				<a href="#"><i class="fa fa-pinterest-square fa-3x"></i></i></a>
+				<a href="#"><i class="fa fa-tumblr-square fa-3x"></i></a>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
