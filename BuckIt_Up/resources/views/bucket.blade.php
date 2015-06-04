0574e0b4852cb40fd932ee8c9dce80d2f508511d
Scott Brau <scottbrau30@gmail.com>
2015-06-04 15:10:21 -0700

    Hard coded input into bucket.blade

--- BuckIt_Up/resources/views/bucket.blade.php
+++ BuckIt_Up/resources/views/bucket.blade.php
@@ -8,47 +8,46 @@
                 <tr>
                     <th>Name</th>
                     <th>Description</th>
-                </tr>
-                
+                </tr>          
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
-                </tr>
-                <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Skydiving</td>
+                    <td>Jump out of an airplane</td>
                 </tr>
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Go to Italy</td>
+                    <td>Visit the Italian Swiss Alps</td>
                 </tr>
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Visit Iceland</td>
+                    <td>See a glacier and hike to a waterfall</td>
                 </tr>
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Go to Burning Man</td>
+                    <td>Wallow in the music festival</td>
                 </tr>
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Go to Monoco</td>
+                    <td>Watch the Monaco F1 Grand Prix</td>
                 </tr>
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Kiss a dolphin</td>
+                    <td>Take a cruise to the Bahamas</td>
                 </tr>
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Kiss Under a waterfall</td>
+                    <td>Honeymoon in Hawaii</td>
                 </tr>
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Cook a 5 star meal</td>
+                    <td>Look to cook at the Codorn Blue in France</td>
                 </tr>
                 <tr class="in-table">
-                    <td>Name</td>
-                    <td>Description</td>
+                    <td>Win an Oscar</td>
+                    <td>Create an award winning film</td>
+                </tr>
+                <tr class="in-table">
+                    <td>Visit the Vatican</td>
+                    <td>Be blessed by the Pope</td>
                 </tr>
             </table>
     </div>
