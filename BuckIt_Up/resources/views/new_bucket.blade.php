4796937eb0fabee158e324a711f78284bdfedfee
Scott Brau <scottbrau30@gmail.com>
2015-06-05 13:31:12 -0700

    cleaned up new_bucket code

--- BuckIt_Up/resources/views/new_bucket.blade.php
+++ BuckIt_Up/resources/views/new_bucket.blade.php
@@ -7,56 +7,67 @@
         <div>
             <form>
                 <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+                
                 <h4>Bucket List Item 1:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox"> 
+                
                 <h4>Bucket List Item 2:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox"> 
+                
                 <h4>Bucket List Item 3:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox">
+                
                 <h4>Bucket List Item 4:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox">
+                
                 <h4>Bucket List Item 5:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox">
+                
                 <h4>Bucket List Item 6:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox">
+                
                 <h4>Bucket List Item 7:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox">
+                
                 <h4>Bucket List Item 8:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox">
+                
                 <h4>Bucket List Item 9:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox">
+                
                 <h4>Bucket List Item 10:</h4>
                 <div>Name: </div>
                 <input type="text" name="title" class="textbox">            
                 <div>Description: </div>
                 <input type="text" name="description" class="textbox">   
+                
                 <div><button>Save</button><button type="reset">Cancel</button></div>
             </form>
         </div>  
