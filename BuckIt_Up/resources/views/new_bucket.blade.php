ab9bd599d4c8d027c395370e0e2babec59f4a543
Scott Brau <scottbrau30@gmail.com>
2015-06-05 10:03:16 -0700

    updated new_bucket.blade

--- BuckIt_Up/resources/views/new_bucket.blade.php
+++ BuckIt_Up/resources/views/new_bucket.blade.php
@@ -1,15 +1,74 @@
 @extends('layout')
 
-@section('content')	
-	<div class="internal-content">
-            <h1>New BuckIt</h1>
-            <h3>Create your BuckIt List</h3> <!-- optional -->
+@section('content') 
+    <div class="internal-content">
+            <h1>Create Bucket</h1>
+            <h3>Get started on your list ...</h3> <!-- optional -->
             <div>
-                <p> 
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                Delectus eos cumque sit in quibusdam, obcaecati nihil magni ex. 
-                Omnis odio quia cum eaque ipsam magni natus quibusdam ratione temporibus quod!
-                </p>
+                {{-- <form action="{{ URL::to('new_bucket') }}" method="POST"> --}}
+                <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+                    <div>Bucket List Item 1:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 2:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 3:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 4:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 5:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 6:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 7:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 8:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 9:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div>Bucket List Item 10:</div>
+                    Name:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <input type="text" name="description"><br><br>
+                    <div><button>Save</button></div>
+                </form>
             </div>  
     </div>
 @endsection
