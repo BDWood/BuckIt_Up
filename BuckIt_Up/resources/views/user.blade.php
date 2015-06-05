6f1704d44c0daa385c4e91847650e23f41db054c
Scott Brau <scottbrau30@gmail.com>
2015-06-05 12:30:01 -0700

    Styled user account page

--- BuckIt_Up/resources/views/user.blade.php
+++ BuckIt_Up/resources/views/user.blade.php
@@ -4,13 +4,8 @@
 	<div class="internal-content">
         <h1>User</h1>
         <h3>Welcome {{ $user->username }}!!</h3>
-        <div>
-            <p> 
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Delectus eos cumque sit in quibusdam, obcaecati nihil magni ex. 
-            Omnis odio quia cum eaque ipsam magni natus quibusdam ratione temporibus quod!
-            </p>
-        </div>  
+        <div class="user_img">Add User Image</div>  
+        <p>Member Since: June 2015</p>
         <div class="delete_user">
             <a href="user/delete">Delete</a> Your User Profile 
             <span class="warning">(*This action cannot be undone*)</span>
