339e1f09260a3d7ee0235c1d8c7a33e1e11a70da
Scott Brau <scottbrau30@gmail.com>
2015-06-05 12:18:24 -0700

    Hard coded the About page

--- BuckIt_Up/resources/views/about.blade.php
+++ BuckIt_Up/resources/views/about.blade.php
@@ -6,23 +6,13 @@
         <h3>What We're About</h3>
         <div>
             <p> 
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Quis unde fugit dolorem nisi officiis voluptates cum? 
-            Qui tenetur similique quae nostrum incidunt voluptates alias fugiat quis, quo! 
-            Rem, maxime veniam.
+            BuckIt Up was founded in June of 2015 by three students particpating in Rockit 
+            Bootcamp (Phoenix, AZ) as a group project. The goal was to create a site where 
+            people could share "BuckIt List" ideas with other people across the world. But 
+            even more than that, this site is a place to inspire and motivate people to achieve 
+            their dreams. 
             </p>
-            <p> 
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Quis unde fugit dolorem nisi officiis voluptates cum? 
-            Qui tenetur similique quae nostrum incidunt voluptates alias fugiat quis, quo! 
-            Rem, maxime veniam.
-            </p>
-            <p> 
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Quis unde fugit dolorem nisi officiis voluptates cum? 
-            Qui tenetur similique quae nostrum incidunt voluptates alias fugiat quis, quo! 
-            Rem, maxime veniam.
-            </p>
+            <h4>Your dreams await ...Turn them into reality.</h4>
         </div>  
     </div>
 @endsection
